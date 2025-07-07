2d28379d0ff98b756164de8eb5a649e70b526bd3
jammy0101 <walker0101walker.gmail.com>
2025-07-07 15:10:13 +0500

    caculation

---
+++ calculation.dart
@@ -0,0 +1,17 @@
+void main(){
+
+int num1 = 10;
+int num2 = 10;
+
+
+int sumation = num1 + num2 ;
+int subtraction = num1 - num2 ;
+int multiplication = num1 * num2;
+double division = num1 / num2;
+
+
+print("THE SUMATION  IS : $sumation");
+print("THE SUBTRACTION IS : $subtraction");
+print("THE MULTIPLICATION IS : $multiplication");
+print("THE DIVISION IS  : $division");
+}
