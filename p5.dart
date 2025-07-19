5e0904a101e7b4388c13e057c8525f1acb97f561
jammy0101 <walker0101walker.gmail.com>
2025-07-19 22:11:34 +0500

    prectice

---
+++ p5.dart
@@ -0,0 +1,11 @@
+void main(){
+//Write a program to find quotient and remainder of two integers.
+int divident = 17;
+int diviser = 5 ;
+
+int  quotiont = divident ~/ diviser ;
+int reminder = divident % diviser ;
+
+print(quotiont);
+print(reminder);
+}
---
+++ p6.dart
@@ -0,0 +1,18 @@
+void main(){
+  //Write a program to swap two numbers.
+
+int number = 2;
+int number1 = 4;
+
+int temp ;
+
+print(number);
+print(number1);
+
+temp = number;
+number = number1;
+number1 = temp;
+
+print(number);
+print(number1);
+}
