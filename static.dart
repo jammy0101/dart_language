687fc4722b8ef0857cf74d3e97b812fb288d970c
jammy0101 <walker0101walker.gmail.com>
2025-09-25 17:37:32 +0500

    about_static

--- static.dart
+++ static.dart
@@ -36,17 +36,7 @@
 //   print(value); // true
 // }
 
-// void main() {
-//   Object obj = "Hello";
 
-//   print(obj.toString()); // ✅ Allowed (toString() is in Object)
-
-//   // print(obj.length); ❌ Error (Object doesn’t know String has length)
-
-//   // Fix with casting:
-//   // print((obj as String).length); // ✅ 5
-//   print((obj as String).length);
-// }
 
 
 // void main() {
